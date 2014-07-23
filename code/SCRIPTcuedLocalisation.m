d0ed7b02d3a54d366d6df5d091416a5ee594b208
Ben Vincent <b.t.vincent@dundee.ac.uk>
2014-07-23 21:34:44 +0100

    reformulated cued localisation model

--- code/SCRIPTcuedLocalisation.m
+++ code/SCRIPTcuedLocalisation.m
@@ -29,7 +29,7 @@ T1=clock;
 switch run_type
     case{'testing'}
         TRIALS				= 100; % number of trials to simulate it any one run
-        cue_validity_list	= linspace(0,1,5);
+        cue_validity_list	= linspace(0.01,1-0.01,5);
         variance_list		= 1./[4  1  0.25];
         
     case{'publication'}
